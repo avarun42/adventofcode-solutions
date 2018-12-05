2c9332fd1188adb177c4ddf4fa0b8ebf6939351f
Varun Arora <varun.arora@vanderbilt.edu>
2018-12-05 04:56:28 -0600

    clean up day 3

--- 2018/day03/part1.cr
+++ 2018/day03/part1.cr
@@ -2,17 +2,11 @@ rectangles = File.read_lines("#{__DIR__}/input.txt").map do |line|
     line.scan(/-?\d+/).map(&.[0].to_i)
 end
 
-pixel_rects = Hash(NamedTuple(x: Int32, y: Int32), Int32).new(0)
-num_overlaps = 0
-rectangles.each do |(_id, start_x, start_y, width, height)|
-    (start_x...(start_x + width)).to_a.each do |x|
-        (start_y...(start_y + height)).to_a.each do |y|
-            pixel_rects[{x: x, y: y}] = pixel_rects[{x: x, y: y}] + 1
-            if pixel_rects[{x: x, y: y}] == 2
-                num_overlaps += 1
-            end
-        end
+pixel_rects = Hash(Tuple(Int32, Int32), Int32).new(0)
+rectangles.each do |( _id, start_x, start_y, width, height )|
+    (start_x...(start_x + width)).to_a.product((start_y...(start_y + height)).to_a) do |x, y|
+        pixel_rects[{x, y}] = pixel_rects[{x, y}] + 1
     end
 end
 
-puts num_overlaps
+puts pixel_rects.values.count(&.>(1))
--- 2018/day03/part2.cr
+++ 2018/day03/part2.cr
@@ -2,15 +2,13 @@ rectangles = File.read_lines("#{__DIR__}/input.txt").map do |line|
     line.scan(/-?\d+/).map(&.[0].to_i)
 end
 
-pixel_rects = {} of NamedTuple(x: Int32, y: Int32) => Array(Int32)
-rectangles.each do |(id, start_x, start_y, width, height)|
-    (start_x...(start_x + width)).to_a.each do |x|
-        (start_y...(start_y + height)).to_a.each do |y|
-            pixel_rects[{x: x, y: y}] = pixel_rects.fetch({x: x, y: y}, [] of Int32) << id
-        end
+pixel_rects = {} of Tuple(Int32, Int32) => Array(Int32)
+rectangles.each do |( id, start_x, start_y, width, height )|
+    (start_x...(start_x + width)).to_a.product((start_y...(start_y + height)).to_a) do |x, y|
+        pixel_rects[{x, y}] = pixel_rects.fetch({x, y}, [] of Int32) << id
     end
 end
 
-ids = rectangles.map { |(id, _)| id }
+ids = rectangles.map { |( id, _ )| id }
 overlap_pixels = pixel_rects.values.select { |ids| ids.size > 1 }.flatten
 puts (ids - overlap_pixels).first
