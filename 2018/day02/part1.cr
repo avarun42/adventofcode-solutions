0e3f55f6265122546512ab3e1e7b58df5a644b29
Varun Arora <varun.arora@vanderbilt.edu>
2018-12-05 05:09:18 -0600

    day 2 using one-liners

--- 2018/day02/part1.cr
+++ 2018/day02/part1.cr
@@ -1,6 +1,5 @@
-twos, threes = File.read_lines("#{__DIR__}/input.txt")
-                   .map { |id| id.split("").group_by(&.itself).transform_values(&.size).invert }
-                   .reduce({0, 0}) { |counts, map|
-                        counts.map_with_index { |count, i| map.has_key?(i+2) ? count + 1 : count }
-                   }
-puts twos * threes
+puts File.read_lines("#{__DIR__}/input.txt")
+         .map { |id| id.split("").group_by(&.itself).transform_values(&.size).invert }
+         .reduce({0, 0}) { |counts, map|
+            counts.map_with_index { |count, i| map.has_key?(i+2) ? count + 1 : count }
+        }.product
--- 2018/day02/part2.cr
+++ 2018/day02/part2.cr
@@ -1,10 +1,10 @@
 require "levenshtein"
 
-words = File.read_lines("#{__DIR__}/input.txt")
-
-first, second = words.combinations(2)
-                     .find(words.first(2)) { |pair| Levenshtein.distance(pair[0], pair[1]) == 1 }
-                     .map(&.chars)
-
-common_chars = first.zip(second).map(&.to_set).select(&.size.==(1)).join(&.first)
-puts common_chars
+puts File.read_lines("#{__DIR__}/input.txt")
+         .combinations(2)
+         .find([] of String) { |( first, second )| Levenshtein.distance(first, second) == 1 }
+         .map(&.chars)
+         .transpose
+         .map(&.uniq)
+         .select(&.size.==(1))
+         .join(&.first)
