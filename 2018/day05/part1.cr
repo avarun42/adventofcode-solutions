2edfe611fc5b0053199f3860c318c252fe4c22f4
Varun Arora <varun.arora@vanderbilt.edu>
2018-12-06 03:38:31 -0600

    clean up day 5

--- 2018/day05/part1.cr
+++ 2018/day05/part1.cr
@@ -2,7 +2,7 @@ polymer = File.read_lines("#{__DIR__}/input.txt")[0].chars
 modified = true
 
 while modified
-    polymer, modified = polymer.reduce({ [] of Char, false }) do |(polymers, modified), cur|
+    polymer, modified = polymer.reduce({ [] of Char, false }) do |( polymers, modified ), cur|
         prev = polymers[-1]?
         (prev && prev.downcase == cur.downcase && prev.lowercase? != cur.lowercase?) ?
             { polymers[0...-1], true } :
--- 2018/day05/part2.cr
+++ 2018/day05/part2.cr
@@ -1,11 +1,11 @@
 base_polymer = File.read_lines("#{__DIR__}/input.txt")[0].chars
 unit_types = base_polymer.map { |unit| { unit.upcase, unit.downcase } }.to_set
 
-min_polymer_length = unit_types.min_of do |(unit_uppercase, unit_lowercase)|
+min_polymer_length = unit_types.min_of do |unit_uppercase, unit_lowercase|
     polymer = base_polymer.reject(&.==(unit_uppercase)).reject(&.==(unit_lowercase))
     modified = true
     while modified
-        polymer, modified = polymer.reduce({ [] of Char, false }) do |(polymers, modified), cur|
+        polymer, modified = polymer.reduce({ [] of Char, false }) do |( polymers, modified ), cur|
             prev = polymers[-1]?
             (prev && prev.downcase == cur.downcase && prev.lowercase? != cur.lowercase?) ?
                 { polymers[0...-1], true } :
